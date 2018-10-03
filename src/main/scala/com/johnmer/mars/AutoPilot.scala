c7fb93b33ddd627ddc25b42240491c2ceeedb3bf
John Merrick <johnmerrick12345@yahoo.com>
2018-10-04 00:50:36 +0100

    Create an AutoPilot class to provide a function to determine the shortest path.
    There are 2 functions, one calculates this distance by measuring the absolute difference between the x and y coordinates
    
    The second function attempts to calculate the shortest path recursively but currently only works when both the x and y coordinates of the destination are both greater than or equal to those of the starting point

--- src/main/scala/com/johnmer/mars/AutoPilot.scala
+++ src/main/scala/com/johnmer/mars/AutoPilot.scala
@@ -6,17 +6,16 @@ case class AutoPilot(grid: Grid) {
     Math.abs(startCoord.x - destCoord.x) + Math.abs(startCoord.y - destCoord.y)
   }
 
-  def findSimpleShortestPathRecursive(startCoord: Coordinate, destCoord: Coordinate): Int = {
+  def findShortestPathRecursive(startCoord: Coordinate, destCoord: Coordinate): Int = {
 
     if (startCoord == destCoord)
       0
     else if (startCoord.x < 0 || startCoord.x >= grid.xBound || startCoord.y < 0 || startCoord.y >= grid.yBound) {
       1000000000
     }
-    else
-    {
-      (findSimpleShortestPathRecursive(Coordinate(startCoord.x + 1, startCoord.y), destCoord) min
-        findSimpleShortestPathRecursive(Coordinate(startCoord.x, startCoord.y + 1), destCoord)
+    else {
+      (findShortestPathRecursive(Coordinate(startCoord.x + 1, startCoord.y), destCoord) min
+        findShortestPathRecursive(Coordinate(startCoord.x, startCoord.y + 1), destCoord)
         ) + 1
     }
   }
